94484acd647d4ba0054195c653b47203c0c734a1
dotmantosh <dotuntosh@gmail.com>
2022-04-26 18:13:54 +0100

    changes made for heroku deployment

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
+/* const serveStatic = require('serve-static') */
 
 const app = express()
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('build'))
+}
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+/* app.use('/', serveStatic(path.join(__dirname, '/dist'))) */
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'build','index.html'))
 })
 
 const port = process.env.PORT || 8080
